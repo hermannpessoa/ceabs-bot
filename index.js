2013d737ab0558257d016af8a3fcab90b45e33d7
hermannpessoa <hermann.pessoa@gmail.com>
2021-06-08 14:13:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const user = require('./auth/auth');
 const path = require('path')
 const fs = require('fs');
 const fsExtra = require('fs-extra');
+const moment = require('moment');
 
 async function robo() {
-    console.log("Iniciando Robo CEABS");
+    console.log("Iniciando Robo CEABS v0.1");
 
     const browser = await puppeteer.launch({
-        headless: false
+        //headless: false
     });
     const page = await browser.newPage();
     const client = await page.target().createCDPSession();
@@ -24,15 +25,6 @@ async function robo() {
         height: 850
     });
 
-    // await page.setRequestInterception(true);
-
-    //  await page.on('request', request => {
-    //     if (request.resourceType() === 'image')
-    //         request.abort();
-    //     else
-    //         request.continue();
-    // });
-
     await page.goto('https://core.ceabs.com.br/CEABS_Common/Login.aspx?OriginalURL=%2fCEABS_App%2fHome.aspx');
 
 
@@ -44,31 +36,36 @@ async function robo() {
     await page.type(selectorPassord, user.password);
     await page.click(submitButton);
 
-    await page.waitForSelector('#DublinTheme_wt23_block_wtHeader_CEABS_Common_wt38_block_DublinTheme_wt1_block_wtHeader_loginInfo_wt16_wtLoginInfo_username2');
+    await page.waitForSelector('#DublinTheme_wt23_block_wtHeader_CEABS_Common_wt38_block_DublinTheme_wt1_block_wtHeader_loginInfo_wt16_wtLoginInfo_username2').then(async() => {
+        
+        console.log("Login efetuado com sucesso");
+        await page.goto('https://core.ceabs.com.br/CEABS_FieldService/WorkOrdersAdvanced.aspx');
+        await filter();
 
-    await page.goto('https://core.ceabs.com.br/CEABS_FieldService/WorkOrdersAdvanced.aspx');
+    });
+    
 
-    const inputDateStart = '#DublinTheme_wt141_block_wtMainContent_WebPatterns_wtFiltersContainer_block_wtContent_WebPatterns_wtFiltros_block_wtColumn2_wtSchedulingPeriodStartContainer';
-    const inputDateEnd = '#DublinTheme_wt141_block_wtMainContent_WebPatterns_wtFiltersContainer_block_wtContent_WebPatterns_wtFiltros_block_wtColumn2_wtSchedulingPeriodEndContainer';
-    const findButton = '#DublinTheme_wt141_block_wtMainContent_WebPatterns_wtFiltersContainer_block_wtContent_wt52';
-    const currentDate = '08/06/2021';
+    async function filter(){
+        const inputDateStart = '#DublinTheme_wt141_block_wtMainContent_WebPatterns_wtFiltersContainer_block_wtContent_WebPatterns_wtFiltros_block_wtColumn2_wtSchedulingPeriodStartContainer';
+        const inputDateEnd = '#DublinTheme_wt141_block_wtMainContent_WebPatterns_wtFiltersContainer_block_wtContent_WebPatterns_wtFiltros_block_wtColumn2_wtSchedulingPeriodEndContainer';
+        const findButton = '#DublinTheme_wt141_block_wtMainContent_WebPatterns_wtFiltersContainer_block_wtContent_wt52';
+        const currentDate = moment().add(1, 'day').format('DD/MM/YYYY');
 
-    // await page.waitForSelector(inputDateStart);
-    // await page.waitForSelector(inputDateEnd);
-    // await page.waitForTimeout(5000);
+        await page.waitForSelector(inputDateStart).then(async () => {
+            await page.click(inputDateStart);
+            await page.keyboard.type(currentDate);
 
-    await page.waitForSelector(inputDateStart).then(async () => {
-        await page.click(inputDateStart);
-        await page.keyboard.type(currentDate);
+            await page.click(inputDateEnd);
+            await page.keyboard.type(currentDate);
+            
 
-        await page.click(inputDateEnd);
-        await page.keyboard.type(currentDate);
+            await page.click(findButton);
 
+            console.log("Buscando OS para o dia: ", currentDate);
 
-        await page.click(findButton);
-
-        getAllandPrint();
-    })
+            getAllandPrint();
+        })
+    }
 
     async function getAllandPrint() {
 
@@ -77,49 +74,69 @@ async function robo() {
             visible: true,
             timeout: 0
         }).then(async () => {
+            
+            const count = await page.evaluate(() => document.querySelector('.Counter_Message').innerHTML.replace('&nbsp;', ' '));
+            console.log('Encontramos: '+count);
 
             const selectAll = '#DublinTheme_wt141_block_wtMainContent_wtWorkOrderTable_ctl02_RichWidgets_wt75_block_wtchkSelectAll';
             await page.waitForTimeout(1000).then(async () => {
                 await page.click(selectAll);
-                print();
+                printButton();
             })
         })
     }
 
-    async function print() {
+    async function printButton() {
+
+        console.log("Gerando PDF das OS para impressão...");
+
         const printLink = "#DublinTheme_wt141_block_wtMainContent_wtlinkPrint3";
         await page.click(printLink);
 
-        await page.waitForTimeout(7000).then(async () => {
-            getFilesFromFolder();
+        await page.waitForTimeout(1000).then(async () => {
+            await getFilesFromFolder();
         })
 
 
         return false;
     }
-    // await page.screenshot({
-    //     path: 'example.png'
-    // });
-    // await browser.close();
 
     async function getFilesFromFolder() {
-        fs.readdir(path.resolve(__dirname, './pdf'), async (err, files) => {
-            if (err)
-                console.log(err);
-            else {
-                console.log("\nCurrent directory filenames:");
-                files.forEach(file => {
-                    console.log('imprimir: ' + file);
-                })
+        let timer = setInterval(function() {
+            
+            fs.readdir(path.resolve(__dirname, './pdf'), async (err, files) => {
+                if (err){
+                    console.log(err);
+                    clearInterval(timer);
+                } else {
+                    
+                    
+                    if(files.length > 0){
+                        console.log("Arquivo encontrado...");
+                        clearInterval(timer);
 
-                await page.waitForTimeout(17000).then(async () => {
-                    console.log("Limpando Diretório");
-                    fsExtra.emptyDirSync(path.resolve(__dirname, './pdf'))
-                })
-            }
-        })
+                        files.forEach(file => {
+                            console.log('imprimir: ' + file);
+                        })
 
-        console.log("Robo CEABS Finalizado");
+                        await page.waitForTimeout(5000).then(async () => {
+                            console.log("Limpando Diretório");
+                            fsExtra.emptyDirSync(path.resolve(__dirname, './pdf'));
+                            console.log("Robo CEABS Finalizado");
+                            await page.waitForTimeout(5000).then(async () => {
+                                await browser.close();
+                            })
+                        })
+
+                       
+                    }else {
+                        console.log('AGUARDE...');
+                    }
+                }
+            })
+        }, 1000 );
+
+        
     }
 }
 
